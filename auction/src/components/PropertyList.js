add67e0b6cddb92619a3cae9eac02c4b3f552ab6
patliemoaloo <aloemo77@gmail.com>
2023-11-08 13:45:05 +0300

    Editing db.json file

--- auction/src/components/PropertyList.js
+++ auction/src/components/PropertyList.js
@@ -23,6 +23,8 @@ function PropertyList() {
     )
   }
 
+  
+
   return (
     <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center">
       {properties.map((property) => (
