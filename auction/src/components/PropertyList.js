4e3d42c4309e5a9452c4bf89aa5fc48daaad68c7
patliemoaloo <aloemo77@gmail.com>
2023-11-08 14:29:02 +0300

    LICENSE

--- auction/src/components/PropertyList.js
+++ auction/src/components/PropertyList.js
@@ -23,8 +23,6 @@ function PropertyList() {
     )
   }
 
-  
-
   return (
     <div className="row row-cols-1 row-cols-md-3 g-4 d-flex justify-content-center">
       {properties.map((property) => (
